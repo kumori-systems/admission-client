25e7f48e90f6dbafd9673676d5db7b2ee48098d6
Jerónimo Pla <jpla@iti.upv.es>
2017-07-13 17:14:14 +0200

    deferred class created

---
+++ src/deferred.ts
@@ -0,0 +1,54 @@
+export class Deferred<T> {
+  public promise: Promise<T>;
+
+  private fate: "resolved" | "unresolved";
+  private state: "pending" | "fulfilled" | "rejected";
+
+  private xresolve: (v: any) => void;
+  private xreject: (r: any) => void;
+
+  constructor() {
+    this.state = "pending";
+    this.fate = "unresolved";
+    this.promise = new Promise((resolve, reject) => {
+      this.xresolve = resolve;
+      this.xreject = reject;
+    });
+    this.promise.then(
+      () => this.state = "fulfilled",
+      () => this.state = "rejected",
+    );
+  }
+
+  public resolve(value?: any) {
+    if (this.fate === "resolved") {
+      throw new Error("Deferred cannot be resolved twice");
+    }
+    this.fate = "resolved";
+    this.xresolve(value);
+  }
+
+  public reject(reason?: any) {
+    if (this.fate === "resolved") {
+      throw new Error("Deferred cannot be resolved twice");
+    }
+    this.fate = "resolved";
+    this.xreject(reason);
+  }
+
+  public isResolved() {
+    return this.fate === "resolved";
+  }
+
+  public isPending() {
+    return this.state === "pending";
+  }
+
+  public isFulfilled() {
+    return this.state === "fulfilled";
+  }
+
+  public isRejected() {
+    return this.state === "rejected";
+  }
+}
